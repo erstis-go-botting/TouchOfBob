c42b430a42ddc11b52cce72ada413f33102cc6c5
alskgj <dimitri.wessels@gmail.com>
2013-11-12 14:27:07 +0100

    Update tools.py

--- toolbox/tools.py
+++ toolbox/tools.py
@@ -1,6 +1,8 @@
-def laufzeit(dorf1, dorf2, einheitentyp):
-    if einheitentyp == "lkav":
-        mod = 10
-    else:
-        mod = 20
-    return math.sqrt((dorf1.x-dorf2.x)**2 + (dorf1.y-dorf2.y)**2)*mod
+import math
+
+def distance(dorf1, dorf2):
+    """
+    Erwartet als Parameter zwei Dörfer und gibt ihre Distanz zurück.
+    """
+    distance = math.sqrt((dorf1.x-dorf2.x)**2 + (dorf1.y-dorf2.y)**2)
+    return distance
